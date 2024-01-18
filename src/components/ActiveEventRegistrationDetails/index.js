928faffcfca84521c5b831788b2cd7ec75626dd3
SUDARSAN MAJHI <1996sudarsan@gamail.com>
2024-01-18 07:20:19 +0000

    solution

--- src/components/ActiveEventRegistrationDetails/index.js
+++ src/components/ActiveEventRegistrationDetails/index.js
@@ -1,87 +1,85 @@
-// Write your code here
 import './index.css'
 
 const registrationStatus = {
-    yetToRegister: 'YET_TO_REGISTER',
-    register: 'REGISTER',
-    registrationsClosed: 'REGISTRATIONS_CLOSED'
+  yetToRegister: 'YET_TO_REGISTER',
+  registered: 'REGISTERED',
+  registrationsClosed: 'REGISTRATIONS_CLOSED',
 }
 
 const ActiveEventRegistrationDetails = props => {
-    const {activeEventRegistrationStatus} = props 
+  const {activeEventRegistrationStatus} = props
 
-    const renderNoActiveEventView = () => (
-        <p className="no-active-event-description">
-            Click on an event, to view its registration details
-        </p>
-    )
+  const renderNoActiveEventView = () => (
+    <p className="no-active-event-description">
+      Click on an event, to view its registration details
+    </p>
+  )
 
-    const renderRegistrationsClosedView = () = (
-        <div className="view-container">
-            <img 
-            src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
-            alt="registration closed"
-            className="registration-closed-image"
-            /> 
-            <h1 className="registrations-closed-image">
-                Registrations are closed now!
-            </h1>
-            <p className="registrations-closed-description">
-                Stay tuned. we will reopen the registrations soon!
-            </p>
-        </div>
-    )
+  const renderRegistrationsClosedView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
+        alt="registrations closed"
+        className="registrations-closed-image"
+      />
+      <h1 className="registrations-closed-heading">
+        Registrations Are Closed Now!
+      </h1>
+      <p className="registrations-closed-description">
+        Stay tuned. We will reopen the registrations soon!
+      </p>
+    </div>
+  )
 
-    const renderRegisteredView = () => (
-        <div className="view-container">
-            <img 
-            src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
-            alt="registered"
-            className="registered-img"
-            />
-            <h1 className="registered-heading">
-                You have already registered for the event
-            </h1>
-        </div>
-    )
+  const renderRegisteredView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
+        alt="registered"
+        className="registered-img"
+      />
+      <h1 className="registered-heading">
+        You have already registered for the event
+      </h1>
+    </div>
+  )
 
-    const renderYetToRegisterView = () => (
-        <div className="view-container">
-            <img 
-            src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
-            alt="registered"
-            className="yet-to-register-image"
-            />
-            <p className="registered-description">
-                A live performance bring so much to your relationship with dance.
-                seeing dance live can often make you fall totally in love with this
-                beautiful art form.
-            </p>
-            <button type="button" className="register-here-button">
-                Register Here
-            </button>
-        </div>
-    )
+  const renderYetToRegisterView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
+        alt="yet to register"
+        className="yet-to-register-image"
+      />
+      <p className="register-description">
+        A live performance brings so much to your relationship with dance.
+        Seeing dance live can often make you fall totally in love with this
+        beautiful art form.
+      </p>
+      <button type="button" className="register-here-button">
+        Register Here
+      </button>
+    </div>
+  )
 
-        const renderActiveEventRegistrationDetails = () => {
-        Switch (activeEventRegistrationStatus) {
-        case registrationStatus.yetToRegister:
+  const renderActiveEventRegistrationDetails = () => {
+    switch (activeEventRegistrationStatus) {
+      case registrationStatus.yetToRegister:
         return renderYetToRegisterView()
-        case registrationStatus.registered:
-            return renderRegisteredView()
-        case registrationStatus.registrationsClosed:
+      case registrationStatus.registered:
+        return renderRegisteredView()
+      case registrationStatus.registrationsClosed:
         return renderRegistrationsClosedView()
-        default:
+      default:
         return renderNoActiveEventView()
-        }
-}
+    }
+  }
 
-        return (
-        <div className="registration-status-container">
-        {renderActiveEventRegistrationDetails()}
-        </div>
-            )
-            
-        }
+  return (
+    <div className="registration-status-container">
+      {renderActiveEventRegistrationDetails()}
+    </div>
+  )
+}
 
 export default ActiveEventRegistrationDetails
